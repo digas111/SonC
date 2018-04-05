5fadd3c818c078ee8b4c0254e3b33335f5497c06
Manuel Martins <armando.m.martins@hotmail.com>
2018-04-05 18:06:49 +0100

    Adicionei os enunciados das funcoes

--- cc/src/sonc/battle/World.java
+++ cc/src/sonc/battle/World.java
@@ -1,5 +1,93 @@
 package sonc.battle;
 
 public class World extends java.lang.Object{
-int ola=0;
+	
+	private int 
+
+	void addMovingObject(MovingObejct added) {
+		
+	}
+	
+	void addShipAt(Ship ship,
+            double x,
+            double y,
+            double heading) {
+		
+	}
+	
+	void addShipAtRandom(Ship ship) {
+		
+		
+	}
+	
+	public Movie battle(java.util.List<Ship> ships) {
+		
+		
+	}
+
+	public static double getCollisionDistance() {
+		
+		
+	}
+	
+	public int getCurrentRound() {
+		
+	}
+	
+	public static double getHeight() {
+		
+	}
+	
+	public static double getMargin() {
+		
+	}
+	
+	java.util.Set<MovingObject> getMovingObjects(){
+		
+		
+	}
+	
+	public static int getRounds() {
+		
+	}
+	
+	public java.util.Set<Ship> getShips(){
+		
+	}
+	
+	public static double getWidth() {
+		
+	}
+	
+	static void setCollisionDistance(double collisionDistance) {
+		
+	}
+	
+	void setCurrentRound(int currentRound) {
+		
+		
+	}
+	
+	static void setHeight(double height) {
+		
+		
+	}
+	
+	static void setMargin(double margin) {
+		
+	}
+	
+	static void setRounds(int rounds) {
+		
+		
+	}
+	
+	static void setWidth(double width) {
+		
+		
+	}
+	
+	void update() {
+		
+	}
 }
